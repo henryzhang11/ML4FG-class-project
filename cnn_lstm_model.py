7cd46d24d1b3c3df0b9c06be54654ee4c0a31ddd
henryzhang1 <66122591+henryzhang11@users.noreply.github.com>
2022-04-08 17:41:02 -0400

    Add files via upload

---
+++ cnn_lstm_model.py
@@ -0,0 +1,80 @@
+import torch
+import torch.nn as nn
+
+class CNN_FC(nn.Module):
+    def get_rnn_fea(input_dim, num_hidden = 128)
+        model = nn.Sequential(nn.Linear(input_dim, num_hidden), 
+                                    nn.ReLu(), 
+                                    nn.PReLU(), 
+                                    nn.BatchNorm1d(num_hidden),
+                                    nn.Dropout(0.5),
+                                    nn.Linear(num_hidden, num_hidden),
+                                    nn.ReLu(),
+                                    nn.PReLU(),
+                                    nn.BatchNorm1d(num_hidden),
+                                    nn.Dropout(0.5))
+        return model
+        # this method codes DBN and returns a DBN network
+            #
+    def get_cnn_network():
+        nbfilter = 101 # the default cnn input length for our case since we have 101 nucleotides windows
+        model = nn.Sequential(nn.Conv1d(input_dim = 4, output_dim = 4, filter_widths = 7, padding = 3),
+                                    nn.ReLu(),
+                                    nn.MaxPool1d(3),
+                                    nn.Dropout(0.5),
+                                    nn.Flatten(),
+                                    nn.Linear(nbfilter, nbfilter),
+                                    nn.ReLu(),
+                                    nn.Dropout(0.25))
+        return model
+        # not the same as original code, original code nbfilter = 102
+    
+    def _init_(self): 
+        # this method defines the CNN+fully connected layer network
+        rg_dim = 505 # dim[1] of matrix_regionType.tab
+        rg_hid = 128
+        
+        clip_dim = 3030 # dim[1] of matrix_Cobinding.tab
+        clip_hid = 256
+        
+        rna_dim = 101 # dim[1] of matrix_RNAfold.tab
+        rna_hid = 64
+        
+        cnn_hid = 64
+        
+        motif_dim = 102 # dim[1] of Motif_fea.gz
+        motif_hid = 64
+        
+        seq_hid = 101 # dim[1] of Sequences.fa (here it disagrees with the 
+        #not the same as original code, original code seq_hid=102
+        
+        super(CNN_FC, self)._init_()
+        
+        self.rg_net = get_rnn_fea(rg_dim, rg_hid*2)
+        self.clip_net = get_rnn_fea(clip_dim, clip_hid*3)
+        self.rna_net = get_rnn_fea(rna_dim, rna_hid*2)
+        self.motif_net = get_rnn_fea(motif_dim, motif_hid*2)
+        self.seq_net = get_cnn_network()
+        
+        total_hid=rg_hid*2 + clip_hid*3 + rna_hid*2 + motif_hid*2 + seq_hid # total hid is length of shared representation as mentioned in picturial summary of iDeep
+        # not the same as original code, original code doesn't have "*2"s
+
+        self.lstm_net = nn.Sequential(nn.Dropout(0.5), nn.LSTM(input_size=1, hidden_size=1, num_layers=2, bidirectional=True, batch_first=True))
+
+        self.dense_net=nn.Sequential(nn.Linear(total_hid, 2), nn.Softmax(dim = 0))
+        
+    def forward(self, training_data):
+        #   -training_data: a dictionary containing 5 files: "X_RG" for region type, "X_CLIP" for clip cobinding data, "X_RNA" for RNA structure data, "motif" for motif data, and "seq" for sequence data
+        #                   this model assumes that training_data is preprocessed and split between training and testing
+        rg_net = self.rg_net(training_data["X_RG"])
+        clip_net = self.clip_net(training_data["X_CLIP"])
+        rna_net = self.rna_net(training_data["X_RNA"])
+        motif_net = self.motif_net(training_data["motif"])
+        seq_net = self.seq_net(training_data["seq"])
+        
+        net = torch.cat((rg_net, clip_net, rna_net, motif_net, seq_net),1) # tensors of a batch are concatenated along axis 1 (the only non batch sequence dimension)
+        
+        net = self.lstm_net(net)
+        net = self.dense_net(net)
+        
+        return(net)

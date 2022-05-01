3ca08a475bf78a6d305c53515620b070aa7aff16
henryzhang1 <66122591+henryzhang11@users.noreply.github.com>
2022-05-01 00:09:38 -0400

    Update cnn_transformer.py

--- cnn_transformer.py
+++ cnn_transformer.py
@@ -77,3 +77,143 @@ class CNN_transformer(nn.Module):
         net = torch.cat((net, motif_net), 1)
         net = self.linear(net)
         return(net)
+
+import os
+import torch
+import numpy as np
+
+import torch.nn.functional as F
+from sklearn.preprocessing import OneHotEncoder
+from sklearn import metrics
+import re
+
+import matplotlib.pyplot as plt # for plotting
+import seaborn as sns # also for plotting
+import torch.nn as nn
+
+def train_model(model, dataset, epochs=100, patience=10, verbose = True):
+    
+    # Train a 1D CNN model and record accuracy metrics.
+
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    #model.to(device)
+    #dataset.to(device)
+    proteins = dataset.getProteinList()
+    onehot = OneHotEncoder(handle_unknown="ignore")
+    proteins = np.array(proteins).reshape(1,-1)
+    onehot.fit(proteins)
+    trainLen = int(0.8 * len(dataset))
+    validationLen = len(dataset) - trainLen
+
+    #we do our randomsampling
+    
+    train_dataset,validation_dataset = torch.utils.data.random_split(dataset,(trainLen,validationLen))
+    train_dataloader = torch.utils.data.DataLoader(train_dataset, batch_size=100, num_workers = 0)
+    
+    validation_dataloader = torch.utils.data.DataLoader(validation_dataset, batch_size=100)
+
+    # Instantiates an optimizer for the model. 
+
+    optimizer = torch.optim.Adam(model.parameters(), lr = 1e-3, eps = 1e-7, amsgrad = True)
+
+    # Run the training loop with early stopping. 
+    
+    patience = patience # for early stopping
+    patience_counter = patience
+    best_val_loss = np.inf
+    check_point_filename = 'model_checkpoint.pt' # to save the best model fit to date
+    
+    import timeit
+    start_time = timeit.default_timer()
+    torch.set_grad_enabled(True) # we'll need gradients
+
+    for epoch in range(epochs):
+        start_time = timeit.default_timer()
+
+        #train_loss, train_acc = run_one_epoch(True, train_dataloader, model, optimizer, device,onehot=onehot)
+        #val_loss, val_acc = run_one_epoch(False, validation_dataloader, model, optimizer, device,onehot=onehot)
+
+        train_loss, train_acc = run_one_epoch(True, train_dataloader, model, optimizer, device)
+        val_loss, val_acc, _, _= run_one_epoch(False, validation_dataloader, model, optimizer, device)
+        
+        if val_loss < best_val_loss: 
+           torch.save(model.state_dict(), check_point_filename)
+           best_val_loss = val_loss
+           patience_counter = patience
+        else: 
+           patience_counter -= 1
+           if patience_counter <= 0: 
+                model.load_state_dict(torch.load(check_point_filename)) # recover the best model so far
+                break
+        elapsed = float(timeit.default_timer() - start_time)
+        print("Epoch %i took %.2fs. Train loss: %.4f acc: %.4f. Val loss: %.4f acc: %.4f. Patience left: %i" % 
+              (epoch+1, elapsed, train_loss, train_acc, val_loss, val_acc, patience_counter ))
+
+def run_one_epoch(train_flag, dataloader, model, optimizer, device="cuda",onehot=None):
+    torch.set_grad_enabled(train_flag)
+    model.train() if train_flag else model.eval() 
+
+    losses = []
+    accuracies = []
+    true_y = np.empty(shape = (0))
+    predicted_y = np.empty(shape = (0))
+
+    lossFunc = nn.BCELoss()
+    for (x,y) in dataloader: # collection of tuples with iterator
+
+        #(x, y) = ( x.to(device), y.to(device) ) # transfer data to GPU
+
+        output = model(x) # forward pass
+        output = output.squeeze() # remove spurious channel dimension
+        
+        y = torch.Tensor(list(y))
+        loss = lossFunc(output,y) # numerically stable
+        #loss= nn.NLLLoss()(torch.log(output), torch.Tensor.long(y))
+        if train_flag: 
+            loss.backward() # back propagation
+            optimizer.step()
+            optimizer.zero_grad()
+
+        losses.append(loss.detach().cpu().numpy())
+        accuracy = torch.mean( ( (output > .5) == (y > .5) ).float() )
+        accuracies.append(accuracy.detach().cpu().numpy())
+        if not train_flag:
+            #print(y.detach().numpy())
+            true_y = np.concatenate([true_y, y.detach().numpy()])
+            predicted_y = np.concatenate([predicted_y, output.detach().numpy()])
+
+    # if training, return losses and accuracies
+    if train_flag:
+        return( np.mean(losses), np.mean(accuracies) )
+
+    # if testing, return training, losses, accuracies, predictions, true results, 
+    else:
+        return( np.mean(losses), np.mean(accuracies), true_y, predicted_y)
+
+if __name__ == '__main__':
+    dataDir = "/content/clip/"
+ #full path to the directory holding all of the data 
+    #call the combined dataset class
+    trainingData = CombinedDataset(dataDir,training=True)# we want to load the training set
+    testingData = CombinedDataset(dataDir, training=False)# here we load the testing
+    
+    for p,protein in enumerate(os.listdir(dataDir)):
+        print("Training:{}".format(protein))
+        number_of_clip_experiments = trainingData.loadData(p)#will load in the data for protein located at index p in the directory
+        cnntransformer = CNN_transformer(number_of_clip_experiments)
+        train_model(cnntransformer, trainingData, epochs=30, patience=5, verbose = True)
+
+        # calculate roc curve
+        optimizer = torch.optim.Adam(cnntransformer.parameters(), lr = 1e-3, eps = 1e-7, amsgrad = True)
+        number_of_clip_experiments = testingData.loadData(p)
+        test_dataloader = torch.utils.data.DataLoader(testingData, batch_size=100)
+        _, _, true_y, predicted_y = run_one_epoch(False, test_dataloader, cnntransformer, optimizer)
+
+        # fpr, tpr, threshold = metrics.roc_curve(true_y, predicted_y)
+
+        print("roc_auc scores is %.4f." % metrics.roc_auc_score(true_y, predicted_y))
+
+        for i in range(len(predicted_y)):
+          predicted_y[i] = int(predicted_y[i] > 0.5)
+
+        print("accuracy score is %.4f." % metrics.accuracy_score(true_y, predicted_y))
